879a1ac68309b07da41de19557f3e19d1a86db4a
J. Weir <john@smokingun.com>
2009-10-07 14:47:40 -0400

    Parses the nyc.gov javascript arrays and dumps out the datasets into the data directory.

---
+++ parse_data.js
@@ -0,0 +1,128 @@
+/*
+  Javascript (using Narwhal + Rhino) for parsing the list of NYC.gov datasets.
+  make sure that Narwhal js is in your path
+  http://narwhaljs.org/quick-start.html
+*/
+
+load("lib/json2.js");
+importPackage(java.io);
+
+(function(){
+  // 10 fields
+  // via http://www.nyc.gov/html/datamine/html/data/../../includes/scripts/nav_nodes.js
+  var dataSetIndex = [
+    "category",
+    "dataSets",
+    "description",
+    "agency",
+    "url",
+    "fileName",
+    "fileType",
+    "dateInfo",
+    "keywords",
+    "related"];
+
+  function get(name){
+    return readUrl("http://www.nyc.gov/html/datamine/includes/scripts/"+name+".js");
+  }
+
+  function parse(source){
+    eval(source);
+    for (var i=0; i < datasets.length; i++) {
+      map(datasets[i])
+    };
+  }
+
+  function map(array){
+    var category = array[0],
+        dataset = {};
+
+    // Start at 1, skip category
+    for (var i=1; i < dataSetIndex.length; i++) {
+      dataset[dataSetIndex[i]] = array[i];
+    };
+
+    datasets[category] = datasets[category] || [];
+    datasets[category].push(dataset)
+  }
+
+  var datasets = {};
+
+  parse(get("raw"));
+  parse(get("geo"));
+
+  this.datasets = datasets;
+
+})();
+
+(function(){
+  var out = "";
+
+  function sorted_categories(){
+    var categories = [];
+    for(var category in datasets){
+      categories.push(category);
+    }
+    return (categories.sort());
+  }
+
+  var categories = sorted_categories();
+
+  function title(set){
+    return [" ", set.dataSets, " (", set.fileType,")", "\n"].join("");
+  }
+
+  function fields(set, additionalFields){
+    for (var i=0; i < additionalFields.length; i++) {
+      out += "   "+set[additionalFields[i]]+"\n";//["  ", set[additionalFields[i]]].join("\n");
+    };
+    if(additionalFields.length > 0){
+      out += "\n";
+    }
+  }
+
+  //Converts the dataset into a string
+  //call with an array of additional fields to include that data
+  function text(additionalFields){
+    var sets;
+
+    for (var x=0; x < categories.length; x++) {
+      var category = categories[x]
+
+      out += category+"\n";
+      sets = datasets[category];
+
+      for (var i=0; i < sets.length; i++) {
+        out += title(sets[i]);
+        fields(sets[i], additionalFields);
+      };
+      out += "\n"
+    }
+
+    return out;
+  }
+  function json(){
+    return JSON.stringify(datasets, null, "\t");
+  }
+  // Saves the text to file
+  function save(format, fileName, additionalFields){
+    additionalFields = additionalFields || [];
+
+    var file = new java.io.FileWriter(fileName);
+    try
+    {
+      file.write(this[format](additionalFields));
+    }
+    finally
+    {
+      file.close();
+    }
+  }
+
+  this.json = json;
+  this.text = text;
+  this.save = save;
+})();
+
+save("text","data/nyc_data_sets.txt", ["url"]);
+save("json","data/nyc_data_sets.json");
