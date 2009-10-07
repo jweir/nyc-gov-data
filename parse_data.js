64412efa610abfe6d6a82b3dfe6f9ce79b49eb65
J. Weir <john@smokingun.com>
2009-10-07 15:32:12 -0400

    Really need to test markdown locally.

--- parse_data.js
+++ parse_data.js
@@ -88,10 +88,9 @@ importPackage(java.io);
 
       for (var i=0; i < sets.length; i++) {
         var set = sets[i];
-        out += "* #### ["+set.dataSets+"]("+set.url+") ####  \n";
+        out += "#### ["+set.dataSets+"]("+set.url+")  \n";
         out += "  "+set.description+"  \n";
-        out += "  _format:"+set.fileType+"  updated:"+set.dateInfo+"_  \n";
-        out += "\n"
+        out += "  _format:"+set.fileType+"  updated:"+set.dateInfo+"_  \n\n";
       };
       out += "\n"
     }
